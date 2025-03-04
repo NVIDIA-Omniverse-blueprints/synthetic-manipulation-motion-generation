f7a29ea0bd80cb1b39791c087221cb71fc26dee5
sheikh-nv <sabdulajees@nvidia.com>
2025-03-04 04:10:58 +0000

    Add shader cache file

--- launch.sh
+++ launch.sh
@@ -3,6 +3,8 @@
 # Exits if error occurs
 set -e
 
+curl -s -L https://download.isaacsim.omniverse.nvidia.com/launchable/synthetic-motion-generation/kitcache.tar.gz | tar xzvf - -C /isaac-sim/kit/cache
+
 /isaac-sim/kit/python/bin/python3 -m pip install jupyter
 
 ./_isaac_sim/python.sh -m jupyter lab /workspace/isaaclab/notebook/generate_dataset.ipynb --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.default_url='/tree/generate_dataset.ipynb'
